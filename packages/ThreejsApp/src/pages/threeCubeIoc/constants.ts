e21146b6c08682b74f7c8210e5d170642af3b17f
break_happy <break_happy@163.com>
2023-03-20 14:21:01 +0800

    feat:update

--- packages/ThreejsApp/src/pages/threeCubeIoc/constants.ts
+++
@@ -1,8 +0,0 @@
-const SERVICE_IDENTIFIER = {
-  AddCubeService: Symbol('AddCubeService'),
-  ServiceA: Symbol.for('ServiceA'),
-  AppClass: Symbol.for('AppClass'),
-
-};
-
-export { SERVICE_IDENTIFIER };
