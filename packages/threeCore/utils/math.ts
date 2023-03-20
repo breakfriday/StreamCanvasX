67160e34246127158528b71ad57fea1caffc5862
break_happy <break_happy@163.com>
2023-03-20 14:21:01 +0800

    feat:update

--- packages/threeCore/utils/math.ts
+++
@@ -1,9 +0,0 @@
-import { Box3, Object3D } from 'three';
-
-export function computeAABB(object3D: Object3D): Box3 {
-
-  object3D.updateWorldMatrix(true, true);
-  const aabb = new Box3().expandByObject(object3D);
-  return aabb;
-}
- 
