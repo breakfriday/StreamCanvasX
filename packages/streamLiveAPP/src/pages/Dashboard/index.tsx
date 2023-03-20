41202988d12b501d7f6a8a6ce7af542e5d8bcc25
break_happy <break_happy@163.com>
2023-03-20 18:36:51 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/Dashboard/index.tsx
+++ packages/streamLiveAPP/src/pages/Dashboard/index.tsx
@@ -7,9 +7,22 @@ const { Cell } = ResponsiveGrid;
 const Dashboard = () => {
   return (
     <ResponsiveGrid gap={20}>
-      <Cell colSpan={12}>
-        <Guide />
-      </Cell>
+      <div id="original-player">
+        <video
+          width="300"
+          height="300"
+          id="video"
+          controls="true"
+          preload="none"
+          data-playback-id="29302SySXJgsLjQmbGiGiRQvEgiRplfvU"
+          data-poster-time="10"
+        />
+      </div>
+
+
+      <div id="canvas-container">
+        <canvas id="canvas" width="300" height="300" />
+      </div>
     </ResponsiveGrid>
   );
 };
