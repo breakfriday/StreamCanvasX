6ba1621398b410fe3c91ba8f762aea6ebda20c5d
break_happy <break_happy@163.com>
2023-03-20 19:11:06 +0800

    add:player

--- packages/streamLiveAPP/src/pages/Dashboard/index.tsx
+++ packages/streamLiveAPP/src/pages/Dashboard/index.tsx
@@ -1,14 +1,24 @@
-import * as React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { ResponsiveGrid } from '@alifd/next';
 import Guide from './components/Guide';
+import player from 'StreamCanvasX/player';
 
 const { Cell } = ResponsiveGrid;
 
 const Dashboard = () => {
+  const vedio_ref = useRef(null);
+  useEffect(() => {
+    if (vedio_ref) {
+      const el = vedio_ref?.current;
+      player()
+   
+    }
+  }, []);
   return (
     <ResponsiveGrid gap={20}>
       <div id="original-player">
         <video
+          ref={vedio_ref}
           width="300"
           height="300"
           id="video"
