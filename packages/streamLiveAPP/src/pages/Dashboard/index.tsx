f0b1843fb247e19ac6fd01db18eeb18b2fb3311d
break_happy <break_happy@163.com>
2023-03-20 19:26:16 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/Dashboard/index.tsx
+++ packages/streamLiveAPP/src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { ResponsiveGrid } from '@alifd/next';
 import Guide from './components/Guide';
 import player from 'StreamCanvasX/player';
+import canvasPlayer from 'StreamCanvasX/canvasPlayer';
 
 const { Cell } = ResponsiveGrid;
 
@@ -10,12 +11,13 @@ const Dashboard = () => {
   useEffect(() => {
     if (vedio_ref) {
       const el = vedio_ref?.current;
-      player()
-   
+      player();
+
+      new canvasPlayer();
     }
   }, []);
   return (
-    <ResponsiveGrid gap={20}>
+    <div gap={20}>
       <div id="original-player">
         <video
           ref={vedio_ref}
@@ -30,10 +32,11 @@ const Dashboard = () => {
       </div>
 
 
+
       <div id="canvas-container">
         <canvas id="canvas" width="300" height="300" />
       </div>
-    </ResponsiveGrid>
+    </div>
   );
 };
 
