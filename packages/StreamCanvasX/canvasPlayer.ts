4c58bd55fb8541bb98cc7b3ad3f01297516724c6
break_happy <break_happy@163.com>
2023-03-24 04:55:19 +0800

    feat:update

--- packages/StreamCanvasX/canvasPlayer.ts
+++ packages/StreamCanvasX/canvasPlayer.ts
@@ -1,8 +1,14 @@
 class Processor {
+  private video!: HTMLVideoElement;
+  private canvas!: HTMLCanvasElement;
+  private context!: CanvasRenderingContext2D;
+
   constructor(parmams: { vedio_el: HTMLVideoElement; canvas_el: HTMLCanvasElement }) {
     this.video = parmams.vedio_el;
     this.canvas = parmams.canvas_el;
-    this.context = this.canvas.getContext('2d');
+    if (this.canvas) {
+      this.context = this.canvas.getContext('2d')!;
+    }
 
     this.video.addEventListener(
       'play',
