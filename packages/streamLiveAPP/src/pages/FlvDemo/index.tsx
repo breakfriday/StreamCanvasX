4c290e5b1ad47055d9081310a5d5458a5bea96a8
break_happy <break_happy@163.com>
2023-03-27 12:10:42 +0800

    feat:update

---
+++ packages/streamLiveAPP/src/pages/FlvDemo/index.tsx
@@ -0,0 +1,14 @@
+import React, { useRef, useEffect } from 'react';
+import { Button, Divider, Message, Box, Tab } from '@alifd/next';
+
+
+const FlvDemo = () => {
+  return (
+    <div>
+      this is the flv tes
+
+    </div>
+  );
+};
+
+export default FlvDemo;
