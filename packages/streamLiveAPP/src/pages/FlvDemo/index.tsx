d0b0802d5d7e0d2666839b33eae4ad83ddd8ed5c
break_happy <break_happy@163.com>
2023-03-27 12:44:33 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/FlvDemo/index.tsx
+++ packages/streamLiveAPP/src/pages/FlvDemo/index.tsx
@@ -1,11 +1,14 @@
 import React, { useRef, useEffect } from 'react';
 import { Button, Divider, Message, Box, Tab } from '@alifd/next';
+import JessibucaPlayer from 'react-jessibuca';
 
 
 const FlvDemo = () => {
   return (
     <div>
-      this is the flv tes
+      this is the flv AiTest
+
+      <div>this is  the test</div>
 
     </div>
   );
