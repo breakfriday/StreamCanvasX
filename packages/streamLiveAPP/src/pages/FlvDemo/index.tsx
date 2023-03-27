6b043238ef84770766b4f107f2d2b12372f13f6a
break_happy <break_happy@163.com>
2023-03-28 06:43:25 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/FlvDemo/index.tsx
+++ packages/streamLiveAPP/src/pages/FlvDemo/index.tsx
@@ -9,6 +9,7 @@ const FlvDemo = () => {
       this is the flv AiTest
 
       <div>this is  the test</div>
+      <JessibucaPlayer width={400} height={300} src="http://192.168.100.66:41012/rtp/643F57E0.live.flv" decoder="http://localhost:3333/decoder.js" />
 
     </div>
   );
