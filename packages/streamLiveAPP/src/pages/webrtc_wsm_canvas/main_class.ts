cd8719f2eda286cd262f0c54863438c46b4cfdd9
break_happy <break_happy@163.com>
2023-03-27 14:32:56 +0800

    feat:update

---
+++ packages/streamLiveAPP/src/pages/webrtc_wsm_canvas/main_class.ts
@@ -0,0 +1,12 @@
+import loadWASM from './load_wasm.js';
+import * as tf from '@tensorflow/tfjs';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+
+
+class Main_stream {
+  constructor() {
+
+    console.log("222")
+
+  }
+}
