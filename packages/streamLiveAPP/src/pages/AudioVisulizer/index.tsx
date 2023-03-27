e5c37015e719ff30ecc74cf6436945701caef077
break_happy <break_happy@163.com>
2023-03-28 07:01:03 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/AudioVisulizer/index.tsx
+++ packages/streamLiveAPP/src/pages/AudioVisulizer/index.tsx
@@ -9,20 +9,29 @@ const AudioPage = () => {
   const audio_ref = useRef(null);
   const canvas_ref = useRef(null);
 
+
+  const set_audio = (parm) => {
+    const { src } = parm;
+    if (audio_ref?.current) {
+      const audio_el = audio_ref.current;
+      audio_el.src = src;
+      audio.load();
+      audio.play();
+    }
+  };
+
   return (
     <div >
       <input
         type="file"
         accept="audio/*"
-        onChange={(files) => {
-          var files = files;
-          debugger
-          audio.src = URL.createObjectURL(files[0]);
-          audio.load();
-          audio.play();
+        onChange={(event) => {
+          const files_data = event?.target?.files[0];
+
+          set_audio({ src: URL.createObjectURL(files_data) });
         }}
       />
-      <audio id="audio" controls />
+      <audio id="audio" controls ref={audio_ref} />
       <Divider />
       <canvas ref={canvas_ref} id="canvas" width="800" height="800" />
     </div>
