64021eecf694530f2bf2f0d230c3ee367e17ddeb
break_happy <break_happy@163.com>
2023-03-28 11:37:17 +0800

    feat:update

--- packages/StreamCanvasX/canvasAudioVisulizer.ts
+++ packages/StreamCanvasX/canvasAudioVisulizer.ts
@@ -3,6 +3,8 @@ class CanvasAudioVisulizer_Processor {
   private canvas!: HTMLCanvasElement;
   private audioContext!: AudioContext;
   private context!: CanvasRenderingContext2D;
+  private dataArray: any;
+  private analyserNode: AnalyserNode;
 
   constructor(parmams: { audio_el: HTMLAudioElement; canvas_el: HTMLCanvasElement }) {
     this.audio = parmams.audio_el;
@@ -20,15 +22,28 @@ class CanvasAudioVisulizer_Processor {
     this.audio.play();
 
 
-    // 创建了一个新的 AnalyserNode 对象
-    const analyser = this.audioContext.createAnalyser();
+    // 创建了一个新的 AnalyserNode 对象 ,这是一个 Web Audio API 中的节点，它可以将音频数据转换为频域或时域数据，以便进行可视化
+    this.analyserNode = this.audioContext.createAnalyser();
+    this.analyserNode.fftSize = 2048;
 
     //  创建了一个 MediaElementSourceNode 对象 .  关联HTMLMediaElement. 这可以用来播放和处理来自<video>或<audio> 元素的音频.
-    const audioSrc = this.audioContext.createMediaElementSource(this.audio);
+    const sourceNode = this.audioContext.createMediaElementSource(this.audio);
 
     // AudioContext 的输出连接到 AnalyserNode 的输入，将 HTML5 Audio 元素与 AnalyserNode 的输入连接，以便在 AnalyserNode 中处理音频数据。
-    audioSrc.connect(analyser);
-    analyser.connect(this.audioContext.destination);
+    sourceNode.connect(this.analyserNode);
+    this.analyserNode.connect(this.audioContext.destination);
+
+    const bufferLength = this.analyserNode.frequencyBinCount;
+    this.dataArray = new Uint8Array(bufferLength);
+  }
+
+  visulizerDraw() {
+    const x = 0;
+    const requestAnimationFrame = () => {
+      this.analyserNode.getByteFrequencyData(this.dataArray);
+    };
+
+    requestAnimationFrame();
   }
 }
 
