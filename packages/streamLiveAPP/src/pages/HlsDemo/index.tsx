9b6071026a6698727c66d9920316ced4bf501ad4
break_happy <break_happy@163.com>
2023-04-19 14:31:27 +0800

    feat:update

--- packages/streamLiveAPP/src/pages/HlsDemo/index.tsx
+++ packages/streamLiveAPP/src/pages/HlsDemo/index.tsx
@@ -1,17 +1,17 @@
 import React, { useRef, useEffect } from 'react';
 import { ResponsiveGrid, Divider, Box, Button } from '@alifd/next';
-import player from 'StreamCanvasX/player';
 import CanvasPlayerByVideos from 'StreamCanvasX/canvasPlayerByVideo';
 import mpegts from 'mpegts.js';
 import Hls from 'hls.js';
 import CanvasAudioVisulizer_Processor from 'StreamCanvasX/canvasAudioProcess';
+import fpget from 'lodash/fp/get';
 
 const { Cell } = ResponsiveGrid;
 
 const HlsDemo = () => {
-  const vedio_hls_ref = useRef(null);
-  const veido_flv_ref = useRef(null);
-  const canvas_ref = useRef(null);
+  const vedio_hls_ref = useRef<HTMLMediaElement | null>(null);
+  const veido_flv_ref = useRef<HTMLMediaElement | null>(null);
+  const canvas_ref = useRef<HTMLCanvasElement | null>(null);
   const canvas_audio_ref = useRef(null);
   const mediaSource = new MediaSource();
 
@@ -42,55 +42,6 @@ const HlsDemo = () => {
         const audio_process = new CanvasAudioVisulizer_Processor({ media_el: veido_flv_ref.current!, canvas_el: canvas_audio_ref.current! });
         audio_process.visulizerDraw3();
       });
-    }
-  };
-
-  const get_audio_stream = () => {
-    const video_el = veido_flv_ref?.current;
-    if (video_el) {
-      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-      const elementSourceNode = audioContext.createMediaElementSource(video_el);
-      const analyserNode = audioContext.createAnalyser();
-
-
-      elementSourceNode.connect(analyserNode);
-      analyserNode.connect(audioContext.destination);
-
-      const bufferLength = analyserNode.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-
-      console.info(dataArray);
-      const canvas_audio_el = canvas_audio_ref.current!;
-      const canvasContext = canvas_audio_el.getContext('2d');
-
-
-      const draw_audio = () => {
-        requestAnimationFrame(draw_audio);
-
-        // 获取音频数据
-        analyserNode.getByteFrequencyData(dataArray);
-
-        // 清除canvas
-        canvasContext.fillStyle = 'rgb(255, 255, 255)';
-        canvasContext.fillRect(0, 0, canvas_audio_el.width, canvas_audio_el.height);
-
-        // 设置绘制音频数据的样式
-        const barWidth = (canvas_audio_el.width / bufferLength) * 2.5;
-        let barHeight;
-        let x = 0;
-
-        for (let i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i] / 2;
-
-          canvasContext.fillStyle = 'rgb(0, 0, 0)';
-          canvasContext.fillRect(x, canvas_audio_el.height - barHeight, barWidth, barHeight);
-
-          x += barWidth + 1;
-        }
-      };
-
-      draw_audio();
     }
   };
 
@@ -133,7 +84,7 @@ const HlsDemo = () => {
     }
   };
   return (
-    <div gap={20}>
+    <div>
       <input
         type="file"
         id="file-input"
@@ -143,8 +94,10 @@ const HlsDemo = () => {
           if (files_data) {
             const data_url = URL.createObjectURL(files_data);
             const videoPlayer = veido_flv_ref?.current;
-            videoPlayer.src = data_url;
-            videoPlayer.load();
+            if (videoPlayer) {
+              videoPlayer.src = data_url;
+              videoPlayer.load();
+            }
           }
         }}
       />
@@ -158,12 +111,11 @@ const HlsDemo = () => {
             ref={vedio_hls_ref}
             width="300"
             height="300"
-            controls="true"
+            controls
             preload="none"
           />
           <div>
             <Button
-              type=""
               onClick={() => {
                 hls_play();
               }}
@@ -182,13 +134,12 @@ const HlsDemo = () => {
             width="300"
             height="300"
             id="video2"
-            controls="true"
+            controls
             preload="none"
           />
 
           <div>
             <Button
-              type=""
               onClick={() => {
                 flv_play();
               }}
--- packages/streamLiveAPP/src/routes.ts
+++ packages/streamLiveAPP/src/routes.ts
@@ -3,7 +3,7 @@ import BasicLayout from '@/layouts/BasicLayout';
 
 const Dashboard = lazy(() => import('@/pages/liveDemoPage'));
 const WsmCanvas = lazy(() => import('@/pages/webrtc_wsm_canvas'));
-const FlvDemo = lazy(() => import('@/pages/FlvDemo'));
+
 const AudioVisulizer = lazy(() => import('@/pages/AudioVisulizer'));
 
 const HlsDemo = lazy(() => import('@/pages/HlsDemo'));
@@ -22,11 +22,7 @@ const routerConfig: IRouterConfig[] = [
         exact: true,
         component: WsmCanvas,
       },
-      {
-        path: '/flvDemo',
-        exact: true,
-        component: FlvDemo,
-      },
+    
       {
         path: '/audioVisulizer',
         exact: true,
