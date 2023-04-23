27beac08c5f8e95e0fa5dd523578b661cb2e420a
break_happy <break_happy@163.com>
2023-04-23 19:52:38 +0800

    feat:update to ice3

---
+++ packages/StreamLive/src/menuConfig.tsx
@@ -0,0 +1,37 @@
+import { TableOutlined, WarningOutlined, FormOutlined, DashboardOutlined } from '@ant-design/icons';
+import type { MenuDataItem } from '@ant-design/pro-layout';
+
+const asideMenuConfig: MenuDataItem[] = [
+  {
+    name: 'StreamCanvasX',
+    icon: <WarningOutlined />,
+    children: [
+      {
+        name: 'flv&hls',
+        path: '/success',
+      },
+      {
+        name: '404',
+        path: '/404',
+      },
+    ],
+  },
+  {
+    name: '工作台',
+    path: '/',
+    icon: <DashboardOutlined />,
+  },
+  {
+    name: '表单',
+    path: '/form',
+    icon: <FormOutlined />,
+  },
+  {
+    name: '列表',
+    path: '/list',
+    icon: <TableOutlined />,
+  },
+
+];
+
+export { asideMenuConfig };
