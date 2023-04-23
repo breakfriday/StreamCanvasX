411b5a2eebb4833bebb0ca9f2adf7e990fec91ce
break_happy <break_happy@163.com>
2023-04-23 17:44:43 +0800

    feat:update

--- packages/StreamCanvasX/serviceFactories/create_test_origin_class.ts
+++ packages/StreamCanvasX/serviceFactories/create_test_origin_class.ts
@@ -8,9 +8,9 @@ container1.bind<ServiceA>(ServiceA).toSelf();
 
 
 // 创建一个工厂函数来创建 createOriginSevice 实例
-function createOriginSevice(): AppClass {
+function createOriginServiceInstance(): AppClass {
   return container1.get<AppClass>(AppClass);
 }
 
 
-export default createOriginSevice;
+export default createOriginServiceInstance;
