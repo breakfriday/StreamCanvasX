f309655b079d3c7a6b3939888574a723e9344c66
break_happy <break_happy@163.com>
2023-04-23 20:49:47 +0800

    feat:update

---
+++ packages/StreamLive/src/pages/simpleDemoPage/index.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from 'react';
+import player from 'StreamCanvasX/player';
+import CanvasPlayer from 'StreamCanvasX/canvasPlayer';
+
+
+const SimplePage = () => {
+  const vedio_ref = useRef(null);
+  const canvas_ref = useRef(null);
+  useEffect(() => {
+    if (vedio_ref) {
+      const el = vedio_ref?.current;
+      if (el) {
+        player(el);
+      }
+
+      const canvas_player = new CanvasPlayer({ canvas_el: canvas_ref?.current, vedio_el: vedio_ref?.current });
+    }
+  }, []);
+  return (
+    <div gap={20}>
+      <div id="original-player">
+        <video
+          ref={vedio_ref}
+          width="300"
+          height="300"
+          id="video"
+          controls="true"
+          preload="none"
+          data-playback-id="29302SySXJgsLjQmbGiGiRQvEgiRplfvU"
+          data-poster-time="10"
+          loop
+        />
+      </div>
+
+
+      <div id="canvas-container">
+        <canvas ref={canvas_ref} id="canvas" width="800" height="800" />
+      </div>
+
+
+      <div >this is the test</div>
+
+    </div>
+  );
+};
+
+export default SimplePage;
