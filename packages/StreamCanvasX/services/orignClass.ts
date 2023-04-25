9289a5c906138a00e7842ddaf54b6947cbcda88e
break_happy <break_happy@163.com>
2023-04-26 07:20:26 +0800

    feat:update

--- packages/StreamCanvasX/services/orignClass.ts
+++ packages/StreamCanvasX/services/orignClass.ts
@@ -1,11 +1,12 @@
 import { injectable, inject, Container } from 'inversify';
 import { ServiceA } from './ServiceA';
+import { TYPES } from '../serviceFactories/symbol';
 
 @injectable()
 export class OriginSerivce implements IOriginSerivce {
   logger: ServiceA;
 
-  constructor(@inject(ServiceA) logger: ServiceA) {
+  constructor(@inject(TYPES.IServiceA) logger: ServiceA) {
     this.logger = logger;
   }
 
