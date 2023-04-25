79dc1ccdaf543cf55e8a51e0d50cc439dcdc13aa
break_happy <break_happy@163.com>
2023-04-26 07:20:26 +0800

    feat:update

--- packages/StreamCanvasX/serviceFactories/create_test_origin_class.ts
+++ packages/StreamCanvasX/serviceFactories/create_test_origin_class.ts
@@ -4,13 +4,17 @@ import { ServiceA } from '../services/ServiceA';
 import { TYPES } from './symbol';
 
 
-container1.bind<OriginSerivce>(OriginSerivce).toSelf();
-container1.bind<ServiceA>(ServiceA).toSelf();
+container1.bind<IOriginSerivce>(TYPES.IOriginSerivce).to(OriginSerivce);
+// container1.bind<IOriginSerivce>(OriginSerivce).toSelf();
+// container1.bind<IServiceA>(ServiceA).toSelf();
+
+
+container1.bind<IServiceA>(TYPES.IServiceA).to(ServiceA);
 
 
 // 创建一个工厂函数来创建 createOriginSevice 实例
 function createOriginServiceInstance(): IOriginSerivce {
-  return container1.get<IOriginSerivce>(OriginSerivce);
+  return container1.get<IOriginSerivce>(TYPES.IOriginSerivce);
 }
 
 
