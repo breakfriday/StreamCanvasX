db4fa95d941aec9a713a8208fbc469747d54a9a8
break_happy <break_happy@163.com>
2023-05-06 11:19:04 +0800

    feat:update

--- packages/StreamCanvasX/serviceFactories/symbol.ts
+++ packages/StreamCanvasX/serviceFactories/symbol.ts
@@ -2,6 +2,8 @@ const TYPES = {
     IOriginSerivce: Symbol.for('IOriginSerivce'),
     IServiceA: Symbol.for('IServiceA'),
     IAudioProcessingService: Symbol.for('IAudioProcessingService'),
+    IMainPlayerService: Symbol.for('IMainPlayerService'),
+
   };
 
   export { TYPES };
