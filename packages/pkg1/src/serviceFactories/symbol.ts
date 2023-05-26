5e015be6fc4345f4a71e065e25c20f48a712dd07
break_happy <break_happy@163.com>
2023-05-26 16:17:54 +0800

    feat:update

--- packages/pkg1/src/serviceFactories/symbol.ts
+++
@@ -1,9 +0,0 @@
-const TYPES = {
-    IOriginSerivce: Symbol.for('IOriginSerivce'),
-    IServiceA: Symbol.for('IServiceA'),
-    IAudioProcessingService: Symbol.for('IAudioProcessingService'),
-    IMainPlayerService: Symbol.for('IMainPlayerService'),
-
-  };
-
-  export { TYPES };
