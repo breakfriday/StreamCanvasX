053713e22244c344ea7b0600d3b6044c475c9abd
break_happy <break_happy@163.com>
2023-05-26 15:18:21 +0800

    feat:update

---
+++ packages/pkg1/.docusaurus/babel.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  presets: [
+    require.resolve('/home/break_happy/project/stream8/node_modules/.pnpm/@docusaurus+core@2.4.1_3cuz37cpupvxy2oj3ncfox3l6e/node_modules/@docusaurus/core/lib/babel/preset.js'),
+    [
+      require.resolve('@babel/preset-react'),
+      {
+        "pragma": "createElement",
+        "pragmaFrag": "Fragment",
+        "runtime": "classic"
+      },
+    ],
+  ],
+};
