25f0905acd26af64b9ede75b289a8815cef2e4cc
break_happy <break_happy@163.com>
2023-05-26 16:03:29 +0800

    feat:update

---
+++ packages/pkg1/src/serviceFactories/index.ts
@@ -0,0 +1,8 @@
+import createOriginServiceInstance from './createOriginServiceInstance';
+import { createAudioProcessingServiceInstance } from './createAudioProcessInstance';
+import { createMainPlayerInstance } from './createMainPlayerInstance';
+export {
+        createOriginServiceInstance,
+        createAudioProcessingServiceInstance,
+        createMainPlayerInstance,
+};
---
+++ packages/pkg1/src/serviceFactories/symbol.ts
@@ -0,0 +1,9 @@
+const TYPES = {
+    IOriginSerivce: Symbol.for('IOriginSerivce'),
+    IServiceA: Symbol.for('IServiceA'),
+    IAudioProcessingService: Symbol.for('IAudioProcessingService'),
+    IMainPlayerService: Symbol.for('IMainPlayerService'),
+
+  };
+
+  export { TYPES };
