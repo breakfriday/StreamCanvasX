fe2e3cdeb89c58a81112dad719d480f1ea809598
break_happy <break_happy@163.com>
2023-05-30 01:46:52 +0800

    feat:update

---
+++ packages/StreamCanvasX/.docusaurus/demo-pages/IcePkgDemo_1472c8.jsx
@@ -0,0 +1,12 @@
+
+import BrowserOnly from '@docusaurus/BrowserOnly';
+export default () => {
+  return (
+    <BrowserOnly>
+      {() => {
+        const Demo = require('/home/break_happy/project/stream8/packages/StreamCanvasX/.docusaurus/demos/IcePkgDemo_1472c8.tsx').default;
+        return <Demo />
+      }}
+    </BrowserOnly>
+  )
+}
