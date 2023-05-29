748c9434a6415bb34b6fd1f7fec41c409658c9f8
break_happy <break_happy@163.com>
2023-05-29 10:49:00 +0800

    feat:update

---
+++ packages/StreamCanvasX/src/container.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+// import Logger from './Logger';
+// import orignClass from './orignClass';
+
+const container1 = new Container();
+
+// container.bind<Logger>(Logger).toSelf();
+// container.bind<orignClass>(orignClass).toSelf();
+
+
+export { container1, Container };
