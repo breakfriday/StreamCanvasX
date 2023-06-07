2e7bd92356a47d6eb16b3433ee038fd0c356471e
break_happy <break_happy@163.com>
2023-06-07 15:36:08 +0800

    feat:httpflvLOADER

--- packages/streamcanvasx/es2017/services/stream/fetch_stream_loader.d.ts
+++ packages/streamcanvasx/es2017/services/stream/fetch_stream_loader.d.ts
@@ -1,10 +1,13 @@
 declare class httpFlvStreamLoader {
     private _requestAbort;
+    private _abortController;
     constructor();
     static isSupported(): boolean;
     get requestAbort(): boolean;
     set requestAbort(value: boolean);
+    get abortController(): AbortController;
     fetchStream(url: string): Promise<void>;
+    abortFetch(): void;
     processStream(reader: ReadableStreamDefaultReader): Promise<void>;
     processFlvChunk(chunk: Uint8Array): void;
     abort(): void;
