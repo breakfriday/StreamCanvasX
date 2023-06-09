bd95eb5634fe0aaa3149cb4911e8bb7e6a3e19c7
break_happy <break_happy@163.com>
2023-06-09 16:59:17 +0800

    feat:update

--- packages/streamcanvasx/.docusaurus/demo-pages/IcePkgDemo_8bcb51.jsx
+++ packages/streamcanvasx/.docusaurus/demo-pages/IcePkgDemo_8bcb51.jsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <BrowserOnly>
       {() => {
-        const Demo = require('/home/break_happy/project/stream8/packages/streamcanvasx/.docusaurus/demos/IcePkgDemo_8bcb51.tsx').default;
+        const Demo = require('/home/break_happy/project/stream12/packages/streamcanvasx/.docusaurus/demos/IcePkgDemo_8bcb51.tsx').default;
         return <Demo />
       }}
     </BrowserOnly>
