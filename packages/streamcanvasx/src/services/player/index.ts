51f5b5501a1ac8687b25ace10fd7f0d765d6c730
break_happy <break_happy@163.com>
2023-06-12 05:16:11 +0800

    feat:update

--- packages/streamcanvasx/src/services/player/index.ts
+++ packages/streamcanvasx/src/services/player/index.ts
@@ -3,15 +3,24 @@ import Emitter from '../../utils/emitter';
 
 import { ServiceA } from '../ServiceA';
 import { TYPES } from '../../serviceFactories/symbol';
+import HttpFlvStreamService from '../stream/fetch_stream_loader';
 
 @injectable()
 class PlayerService extends Emitter {
     logger: ServiceA;
-    constructor(@inject(TYPES.IServiceA) logger: ServiceA) {
+    httpFlvStreamService: HttpFlvStreamService;
+    constructor(
+        @inject(TYPES.IServiceA) logger: ServiceA,
+        @inject(TYPES.IHttpFlvStreamLoader) httpFlvStreamService: HttpFlvStreamService,
+        ) {
         super();
+        this.httpFlvStreamService = httpFlvStreamService;
     }
     log() {
         alert(22);
+    }
+    fetchstrean(url: string) {
+        this.httpFlvStreamService.fetchStream(url);
     }
 }
 
