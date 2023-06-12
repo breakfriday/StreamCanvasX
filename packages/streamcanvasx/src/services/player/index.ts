91aa0ecf8c08953e05db37db0c79fdf7166b2693
break_happy <break_happy@163.com>
2023-06-12 09:04:15 +0800

    feat:update

--- packages/streamcanvasx/src/services/player/index.ts
+++ packages/streamcanvasx/src/services/player/index.ts
@@ -6,11 +6,17 @@ import { TYPES } from '../../serviceFactories/symbol';
 import HttpFlvStreamService from '../stream/fetch_stream_loader';
 import FlvDemuxService from '../demux/flvDemux';
 
+
+function now() {
+    return new Date().getTime();
+}
+
 @injectable()
 class PlayerService extends Emitter {
     logger: ServiceA;
     httpFlvStreamService: HttpFlvStreamService;
     flvVDemuxService: FlvDemuxService;
+    private _startBpsTime?: number;
     constructor(
         @inject(TYPES.IServiceA) logger: ServiceA,
         @inject(TYPES.IHttpFlvStreamLoader) httpFlvStreamService: HttpFlvStreamService,
@@ -35,6 +41,47 @@ class PlayerService extends Emitter {
     play(url: string) {
         this.httpFlvStreamService.fetchStream(url);
     }
+
+        //
+        updateStats(options) {
+            options = options || {};
+
+            if (!this._startBpsTime) {
+                this._startBpsTime = now();
+            }
+
+            if (isNotEmpty(options.ts)) {
+                this._stats.ts = options.ts;
+            }
+
+            if (isNotEmpty(options.buf)) {
+                this._stats.buf = options.buf;
+            }
+
+            if (options.fps) {
+                this._stats.fps += 1;
+            }
+            if (options.abps) {
+                this._stats.abps += options.abps;
+            }
+            if (options.vbps) {
+                this._stats.vbps += options.vbps;
+            }
+
+            const _nowTime = now();
+            const timestamp = _nowTime - this._startBpsTime;
+
+            if (timestamp < 1 * 1000) {
+                return;
+            }
+
+            this.emit(EVENTS.stats, this._stats);
+            this.emit(EVENTS.performance, fpsStatus(this._stats.fps));
+            this._stats.fps = 0;
+            this._stats.abps = 0;
+            this._stats.vbps = 0;
+            this._startBpsTime = _nowTime;
+        }
 }
 
 export default PlayerService;
