f69d9429948b500386d4ec7cdebaff38ed097d79
break_happy <break_happy@163.com>
2023-06-20 11:37:47 +0800

    feat:update

--- packages/StreamLive/src/pages/customerPlayers/aa.tsx
+++ packages/StreamLive/src/pages/customerPlayers/aa.tsx
@@ -4,8 +4,17 @@ import { Divider, Space, Button, Checkbox, Form, Input } from 'antd';
 import { createPlayerServiceInstance } from 'streamcanvasx/src/serviceFactories/index';
 
 
-const VideoComponents = () => {
+const VideoComponents = (props) => {
   useEffect(() => {
+    let { url } = props;
+
+    let player = createPlayerServiceInstance({ url });
+
+    player.createFlvPlayer({});
+
+    let canvas_el = player.canvasVideoService.getCanvas2dEl();
+
+    containerRef.current!.append(canvas_el);
 
 
     // loadMediaEvent();
@@ -14,41 +23,10 @@ const VideoComponents = () => {
   const containerRef = useRef(null);
 
 
-  return (<div >
-    <Form
-      name="basic"
-      autoComplete="off"
-      onFinish={(value: {url: string}) => {
-           let player = createPlayerServiceInstance({ url: value.url });
-
-             player.createFlvPlayer({});
-
-           let canvas_el = player.canvasVideoService.getCanvas2dEl();
-
-           let container = document.getElementById('cont')!;
-           container.append(canvas_el);
-          }}
-    >
-      <Form.Item
-        label="url"
-        name="url"
-      >
-        <Input />
-      </Form.Item>
-
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button
-          type="primary"
-          htmlType="submit"
-        >
-          fetch_play
-        </Button>
-      </Form.Item>
-    </Form>
-
-    <div ref={containerRef} />
-  </div>);
+  return (
+    <div >
+      <div ref={containerRef} />
+    </div>);
 };
 
 export default VideoComponents;
