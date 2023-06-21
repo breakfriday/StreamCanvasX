1b482cf9759afa3d41bce655777436378123f326
break_happy <break_happy@163.com>
2023-06-21 10:32:19 +0800

    feat:update

--- packages/streamcanvasx/.docusaurus/babel.config.js
+++ packages/streamcanvasx/.docusaurus/babel.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   presets: [
-    require.resolve('/home/break_happy/project/stream_20/node_modules/.pnpm/@docusaurus+core@2.4.1_3lwsldy73qonkf7lzjkvnayuta/node_modules/@docusaurus/core/lib/babel/preset.js'),
+    require.resolve('/home/break_happy/project/stream_21/node_modules/.pnpm/@docusaurus+core@2.4.1_3lwsldy73qonkf7lzjkvnayuta/node_modules/@docusaurus/core/lib/babel/preset.js'),
     [
       require.resolve('@babel/preset-react'),
       {
