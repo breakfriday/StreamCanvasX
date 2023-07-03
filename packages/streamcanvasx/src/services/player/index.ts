eb4058c42fce6e84bc4be31042d32b957c80c76f
break_happy <break_happy@163.com>
2023-07-03 10:34:14 +0800

    feat:update

--- packages/streamcanvasx/src/services/player/index.ts
+++ packages/streamcanvasx/src/services/player/index.ts
@@ -19,6 +19,7 @@ import debounce from 'lodash/debounce';
 import throttle from 'lodash/throttle';
 import AudioProcessingService from '../audio/audioContextService';
 
+window.streamCanvasX = '0.1.17';
 function now() {
     return new Date().getTime();
 }
