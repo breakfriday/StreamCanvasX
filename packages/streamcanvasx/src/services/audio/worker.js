63b2d254ecb5b443fcfda75c767cf3f18462b36d
break_happy <break_happy@163.com>
2023-07-11 17:05:45 +0800

    feat:update

--- packages/streamcanvasx/src/services/audio/worker.js
+++ packages/streamcanvasx/src/services/audio/worker.js
@@ -1,14 +1,23 @@
 let offscreenCanvas = '';
 let canvasContext = '';
-let bufferData = '';
+let bufferData = ''; // 缓存区数据
+let bufferDataLength_float32 = ''; // 缓存区长度
 
 onmessage = (event) => {
     switch (event.data.event) {
-        case 'updateBufferData':
+        case 'updateBufferRender':
           // 处理更新 bufferData 的逻辑
-            render_audio_buffer(event);
+            // render_audio_buffer(event);
+
+            render_audio_buffer2(event);
           break;
         // 可以添加其他 case 来处理不同的事件
+        case 'updateBufferData':
+             update_buffer(event);
+          break;
+        case 'renderBuffer':
+
+             render_audio_buffer2();
         default:
           break;
       }
@@ -18,9 +27,11 @@ onmessage = (event) => {
   const update_buffer = (event) => {
     const { dataArray, bufferDataLength } = event.data;
 
+    bufferDataLength_float32 = bufferDataLength;
+
 
     if (bufferData === '') {
-        bufferData = new Float32Array(bufferDataLength);
+        bufferData = new Float32Array(bufferDataLength_float32);
     }
 
 
@@ -30,15 +41,69 @@ onmessage = (event) => {
 
     // Add new data to the end of the buffer
     bufferData.set(dataArray, bufferDataLength - dataArray.length);
+
+
+    // debugger;
   };
 
 
   const render_audio_buffer = (event) => {
+    let { dataArray } = event.data;
+    let { bufferLength } = event.data;
+
+    if (event.data.canvas) {
+        offscreenCanvas = event.data.canvas;
+        canvasContext = offscreenCanvas.getContext('2d');
+    }
+
+    let canvas = offscreenCanvas;
+
+
+    const AnimationFrame = () => {
+        if (this.clear === true) {
+            // This returns the function, effectively stopping the loop
+            return;
+        }
+
+
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            canvasContext.lineWidth = 2;
+            canvasContext.strokeStyle = '#7f0';
+
+            canvasContext.beginPath();
+            const sliceWidth = canvas.width / bufferLength;
+            let x = 0;
+            for (let i = 0; i < bufferLength; i++) {
+                const value = dataArray[i] * canvas.height / 2;
+                const y = canvas.height / 2 + value;
+
+                if (i === 0) {
+                    canvasContext.moveTo(x, y);
+                } else {
+                    canvasContext.lineTo(x, y);
+                }
+
+                x += sliceWidth;
+            }
+            canvasContext.lineTo(canvas.width, canvas.height / 2);
+            canvasContext.stroke();
+
+
+        // Use setTimeout here to loop function call. Adjust the delay time as per your requirement. Here 1000/60 mimics a framerate of 60 FPS, similar to requestAnimationFrame
+        // setTimeout(AnimationFrame.bind(this), 1000 / 30);
+    };
+    AnimationFrame();
+  };
+
+
+  const render_audio_buffer2 = (event) => {
     let animationId = null;
 
 
-    let { dataArray } = event.data;
-    let { bufferLength } = event.data;
+    let dataArray = bufferData;
+
+    let bufferLength = bufferDataLength_float32;
+
 
     if (event.data.canvas) {
         offscreenCanvas = event.data.canvas;
