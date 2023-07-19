d6787ab80c4a64c87136c96c074360f5b93851d3
break_happy <break_happy@163.com>
2023-07-19 14:31:16 +0800

    feat:update

--- packages/StreamLive/src/pages/whipwhep/index.tsx
+++ packages/StreamLive/src/pages/whipwhep/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { Divider, Space, Button, Checkbox, Form, Input, Radio, Switch, Slider, Col, Row } from 'antd';
 // import { HttpFlvStreamLoader } from 'streamcanvasx/es2017/services/stream/fetch_stream_loader';
 import { WHIPClient } from './whis.js';
+import { WHEPClient } from './whep.js';
 
 const Whip = () => {
  const containerRef = useRef(null);
@@ -30,6 +31,29 @@ const Whip = () => {
 
         // Start publishing
         whip.publish(pc, url, token);
+ };
+
+ const runWhep = async () => {
+            // Create peerconnection
+        const pc = window.pc = new RTCPeerConnection();
+
+        // Add recv only transceivers
+        pc.addTransceiver('audio');
+        pc.addTransceiver('video');
+
+        pc.ontrack = (event) => {
+            console.log(event);
+            if (event.track.kind == 'video') document.querySelector('video').srcObject = event.streams[0];
+        };
+
+        // Create whep client
+        const whep = new WHEPClient();
+
+        const url = 'https://whep.test/whep/endpoint';
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtaWxsaWNhc3QiOnsidHlwZSI6IlN1YnNjcmliZSIsInNlcnZlcklkIjoidmlld2VyMSIsInN0cmVhbUFjY291bnRJZCI6InRlc3QiLCJzdHJlYW1OYW1lIjoidGVzdCJ9LCJpYXQiOjE2NzY2NDkxOTd9.ZE8Ftz9qiS04zTKBqP1MHZTOh8dvI73FBraleQM9h1A';
+
+        // Start viewing
+        whep.view(pc, url, token);
  };
     return (
       <>
@@ -78,6 +102,8 @@ const Whip = () => {
           </Form.Item>
         </Form>
 
+
+        <video width={400} controls />
 
       </>
 
