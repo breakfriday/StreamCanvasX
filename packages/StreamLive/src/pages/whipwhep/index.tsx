acdf2818855c4255fa8da90cc05880369efa4b82
break_happy <break_happy@163.com>
2023-07-19 14:25:09 +0800

    feat:update

--- packages/StreamLive/src/pages/whipwhep/index.tsx
+++ packages/StreamLive/src/pages/whipwhep/index.tsx
@@ -6,6 +6,29 @@ import { WHIPClient } from './whis.js';
 const Whip = () => {
  const containerRef = useRef(null);
  const playerRef = useRef(null);
+
+ const runWhip = async () => {
+            // Get mic+cam
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+        // Create peerconnection
+        const pc = new RTCPeerConnection();
+
+        // Send all tracks
+        for (const track of stream.getTracks()) {
+            // You could add simulcast too here
+            pc.addTrack(track);
+        }
+
+        // Create whip client
+        const whip = new WHIPClient();
+
+        const url = 'https://whip.test/whip/endpoint';
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IndoaXAgdGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.jpM01xu_vnSXioxQ3I7Z45bRh5eWRBEY2WJPZ6FerR8';
+
+        // Start publishing
+        whip.publish(pc, url, token);
+ };
     return (
       <>
 
@@ -13,8 +36,12 @@ const Whip = () => {
         <Form
           name="basic"
           autoComplete="off"
+          onFinish={(value) => {
+            let { url, token, type } = value;
+          }}
         >
           <Form.Item
+            initialValue={'https://whip.test/whip/endpoint'}
             label="url"
             name="url"
           >
@@ -29,11 +56,20 @@ const Whip = () => {
             <Input />
           </Form.Item>
 
-          <Form.Item label="renderType" name="audioDrawType" initialValue={'1'}>
+          <Form.Item label="renderType" name="type" initialValue={'1'}>
             <Radio.Group>
               <Radio value="2"> whep </Radio>
               <Radio value="1"> whip </Radio>
             </Radio.Group>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+            >
+              submit
+            </Button>
           </Form.Item>
         </Form>
 
