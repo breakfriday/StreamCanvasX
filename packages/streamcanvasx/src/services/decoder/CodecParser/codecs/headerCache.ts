5f0ed23ea1d6765ae3f921a350ce2ade68e7b168
break_happy <break_happy@163.com>
2023-08-21 09:37:35 +0800

    acc parse

---
+++ packages/streamcanvasx/src/services/decoder/CodecParser/codecs/headerCache.ts
@@ -0,0 +1,85 @@
+
+import {
+    getHeader,
+    setHeader,
+    checkCodecUpdate,
+    reset,
+    enable,
+  } from '../constants';
+
+  export default class HeaderCache {
+    constructor(onCodecHeader, onCodecUpdate) {
+      this._onCodecHeader = onCodecHeader;
+      this._onCodecUpdate = onCodecUpdate;
+      this[reset]();
+    }
+
+    [enable]() {
+      this._isEnabled = true;
+    }
+
+    [reset]() {
+      this._headerCache = new Map();
+      this._codecUpdateData = new WeakMap();
+      this._codecHeaderSent = false;
+      this._codecShouldUpdate = false;
+      this._bitrate = null;
+      this._isEnabled = false;
+    }
+
+    [checkCodecUpdate](bitrate, totalDuration) {
+      if (this._onCodecUpdate) {
+        if (this._bitrate !== bitrate) {
+          this._bitrate = bitrate;
+          this._codecShouldUpdate = true;
+        }
+
+        // only update if codec data is available
+        const codecData = this._codecUpdateData.get(
+          this._headerCache.get(this._currentHeader),
+        );
+
+        if (this._codecShouldUpdate && codecData) {
+          this._onCodecUpdate(
+            {
+              bitrate,
+              ...codecData,
+            },
+            totalDuration,
+          );
+        }
+
+        this._codecShouldUpdate = false;
+      }
+    }
+
+    [getHeader](key) {
+      const header = this._headerCache.get(key);
+
+      if (header) {
+        this._updateCurrentHeader(key);
+      }
+
+      return header;
+    }
+
+    [setHeader](key, header, codecUpdateFields) {
+      if (this._isEnabled) {
+        if (!this._codecHeaderSent) {
+          this._onCodecHeader({ ...header });
+          this._codecHeaderSent = true;
+        }
+        this._updateCurrentHeader(key);
+
+        this._headerCache.set(key, header);
+        this._codecUpdateData.set(header, codecUpdateFields);
+      }
+    }
+
+    _updateCurrentHeader(key) {
+      if (this._onCodecUpdate && key !== this._currentHeader) {
+        this._codecShouldUpdate = true;
+        this._currentHeader = key;
+      }
+    }
+  }
