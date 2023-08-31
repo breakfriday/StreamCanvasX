ac67db4cb549a90a4a1ecf6e3a81f24314e9fb69
Shiyl <734722984@qq.com>
2023-08-31 17:16:01 +0800

    transform build

--- packages/streamcanvasx/.docusaurus/babel.config.js
+++ packages/streamcanvasx/.docusaurus/babel.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   presets: [
-    require.resolve('/home/break_happy/project/stream10/node_modules/.pnpm/@docusaurus+core@2.4.1_3cuz37cpupvxy2oj3ncfox3l6e/node_modules/@docusaurus/core/lib/babel/preset.js'),
+    require.resolve('/home/shiyl/work/streamcanvasx/node_modules/.pnpm/@docusaurus+core@2.4.1_eslint@8.42.0_react-dom@18.2.0_react@18.2.0/node_modules/@docusaurus/core/lib/babel/preset.js'),
     [
       require.resolve('@babel/preset-react'),
       {
