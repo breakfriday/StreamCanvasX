c8422db0caeed4c17953eac350e1baaae8dfe17b
break_happy <break_happy@163.com>
2023-09-13 14:51:54 +0800

    feat:decrypt

--- packages/streamcanvasx/src/services/stream/fetch_stream_loader.ts
+++ packages/streamcanvasx/src/services/stream/fetch_stream_loader.ts
@@ -21,24 +21,8 @@ class HttpFlvStreamLoader {
     private downLoadConfig: {loading: boolean; text: string; startTime: number};
     private streamParser: CodecParser;
 
-    private _runtimeInitializedNotify: () => void;
-    private _runtimeInitialized(): Promise<void> {
-        return new Promise((resolve) => {
-            this._runtimeInitializedNotify = resolve;
-        });
-    }
-
-
-    private _initAlldNotify: () => void;
-    private _initAll(): Promise<void> {
-        return new Promise((resolve) => {
-            this._initAlldNotify = resolve;
-        });
-    }
 
     private sm4Instance: any;
-
-    private wasminit: boolean;
 
 
     private bufferLength: number; // 初始缓冲区长度
@@ -54,7 +38,7 @@ class HttpFlvStreamLoader {
         this.requestAbort = false;
         this.hertTime = 0;
         this.maxHeartTimes = 10;
-        this.wasminit = false;
+
         this.bufferLength = 160;
 
 
@@ -63,69 +47,8 @@ class HttpFlvStreamLoader {
 
         // this.playerService = playerService;
     }
-    // async beforInit() {
-    //     await addScript2('gmssl_zb/gmssl_zb.js');
-
-    //     Module.onRuntimeInitialized = () => {
-    //         this._runtimeInitializedNotify();
-    //     };
 
 
-    //     await this._runtimeInitialized();
-
-
-    //     this.createSm4();
-
-
-    //     this.initAudioPlayer();
-
-
-    //     this._initAlldNotify();
-    // }
-
-    // createSm4() {
-    //     let $this = this;
-    //     let gmssl = {
-
-    //         ondata: function (chunk, size) {
-    //             const asyncProcess = async (chunk, size) => {
-    //                 let vdata = new Uint8Array(Module.HEAPU8.buffer, chunk, size);
-
-
-    //                 let frames = [...$this.streamParser.parseChunk(vdata)];
-
-
-    //                 try {
-    //                       $this.playerService.mseDecoderService.onstream(frames);
-    //                 } catch (e) {
-    //                     debugger;
-    //                 }
-    //             };
-
-    //             asyncProcess(chunk, size);
-
-
-    //             // const pdata = Uint8Array.from(Module.HEAPU8.subarray(chunk, chunk + size));
-    //         },
-
-    //         onend: function () {
-    //             console.log('------------222222222222222-------------------------');
-    //         },
-    //     };
-
-
-    //     this.sm4Instance = new Module.GmsslZb(gmssl);
-    // }
-    // initAudioPlayer() {
-    //         const mimeType = 'audio/aac';
-    //             const options = {
-    //             onCodec: () => {},
-    //             onCodecUpdate: () => {},
-    //             enableLogging: true,
-    //         };
-
-    //         this.streamParser = new CodecParser(mimeType, options);
-    // }
     static isSupported() {
         if (window.fetch && window.ReadableStream) {
              return true;
@@ -207,92 +130,8 @@ class HttpFlvStreamLoader {
         return encoder.encode(str);
     }
 
-    async fetchStream2(): Promise<Uint8Array | null> {
-        let { url } = this.playerService.config;
-        let headers = new Headers();
-        this._abortController = new AbortController();
-        let params: RequestInit = {
-            method: 'GET',
-            mode: 'cors', // cors is enabled by default
-            credentials: 'same-origin', // withCredentials is disabled by default
-            headers: headers,
-            cache: 'default',
-            referrerPolicy: 'no-referrer-when-downgrade',
-            signal: this.abortController.signal,
 
-        };
-
-
-        const response: Response = await fetch(url, params);
-        if (this.requestAbort === true) {
-            response.body?.cancel();
-            return null;
-        }
-
-        const stream = response.body;
-        const reader = stream?.getReader();
-
-        if (!reader) {
-            return null;
-        }
-
-        const chunks: Uint8Array[] = [];
-
-        while (true) {
-            const { value, done } = await reader.read();
-
-            if (done) {
-                break;
-            }
-
-            if (value) {
-                chunks.push(value);
-            }
-        }
-
-        let data = this.concatUint8Arrays(chunks);
-        let startTime = performance.now();
-
-        let dataString = this.bufferToBase64(data.buffer);
-
-
-        let secondTime = performance.now();
-
-        console.log('perforceTime-bufferToBase64', `${secondTime - startTime} milliseconds.`);
-
-
-        let sm4Config = {
-            key: 'JeF8U9wHFOMfs2Y8',
-            mode: 'ecb',
-            iv: 'UISwD9fW6cFh9SNS',
-            cipherType: 'base64',
-        };
-
-
-        let sm4 = new Sm4js(sm4Config);
-
-        let ciphertext = sm4.encrypt(dataString);
-        let thirdTime = performance.now();
-
-        console.log('perforceTime-sm4Encode', `${thirdTime - secondTime} milliseconds`);
-
-
-        let decodeText = sm4.decrypt(ciphertext);
-        let fourthTime = performance.now();
-        console.log('perforceTime-sm4decode', `${fourthTime - thirdTime} milliseconds`);
-
-
-         let buffer2 = this.base64tobuffer(decodeText);
-         let fifthTime = performance.now();
-
-
-         console.log('perforceTime-base64toBuffer', `${fifthTime - fourthTime} milliseconds`);
-
-
-        return data;
-    }
     async fetchStream(): Promise<void> {
-        debugger;
         // try {
         //  await this._initAll();
         // } catch (e) {
@@ -441,118 +280,6 @@ class HttpFlvStreamLoader {
 
     async processStream(reader: ReadableStreamDefaultReader): Promise<void> {
         this.playerService.preProcessing.processStream(reader);
-
-        return false;
-        if (!window.pp) {
-           await this.playerService.mseDecoderService.start();
-
-            window.pp = true;
-        }
-        let $this = this;
-
-        let buffer16 = new Uint8Array(this.bufferLength); // 预设一个大小为16字节的buffer 用来解密
-        let offset = 0;
-        let { streamParser } = this;
-
-
-        let remainingBytes = new Uint8Array(0); // Buffer for bytes that overflow the current chunk
-        let isFirstChunk = true;
-        while (true) {
-            try {
-                const { done, value } = await reader.read();
-
-
-                const chunk = value?.buffer;
-                // console.log(chunk);
-                if (done) {
-                    console.log('Stream complete');
-
-                    return;
-                }
-
-                let concatenated = new Uint8Array(remainingBytes.length + value.length);
-                concatenated.set(remainingBytes);
-                concatenated.set(value, remainingBytes.length);
-
-                if (isFirstChunk) {
-                    if (concatenated.length >= 160) {
-                      //  let firstChunk = concatenated.subarray(0, 160);
-
-                        let firstChunk = new Uint8Array(concatenated.buffer.slice(0, 160));
-
-
-                        // remainingBytes = concatenated.subarray(160);
-
-                        remainingBytes = new Uint8Array(concatenated.buffer.slice(160));
-                        debugger;
-                        $this.sm4Instance.init(firstChunk, 'ideteck_chenxuejian_test');
-                        isFirstChunk = false;
-                    } else {
-                        remainingBytes = concatenated; // If the chunk is smaller than 160 bytes, store and continue
-                        continue;
-                    }
-                } else {
-                    remainingBytes = concatenated;
-                }
-
-                const maxMultipleOf16 = Math.floor(remainingBytes.length / 16) * 16;
-
-
-                if (maxMultipleOf16 > 0) {
-                    // let chunk = remainingBytes.subarray(0, maxMultipleOf16);
-                    let chunkToDecode = remainingBytes.buffer.slice(0, maxMultipleOf16);
-                    debugger;
-                    this.sm4Instance.decode(chunkToDecode);
-                    // remainingBytes = remainingBytes.subarray(maxMultipleOf16);
-
-                    remainingBytes = new Uint8Array(remainingBytes.buffer.slice(maxMultipleOf16));
-
-                    debugger;
-                }
-
-
-                // if ($this.wasminit === false) {
-                //     if(value.le)
-
-                // }
-
-
-                // for (let byte of value) {
-                //     buffer16[offset] = byte;
-                //     offset++;
-
-                //     if (offset === this.bufferLength) {
-                //         // decrypt(buffer); // 当我们有16字节时，传递给解密函数
-
-                //         if ($this.wasminit === false) {
-                //             $this.sm4Instance.init(buffer16.buffer, 'ideteck_chenxuejian_test');
-                //             $this.wasminit = true;
-                //             this.bufferLength = 16 * 1000;
-                //         } else {
-                //             if (window.ppp <= 3000) {
-                //                 $this.sm4Instance.decode(buffer16.buffer);
-                //                 window.ppp++;
-                //             }
-                //         }
-
-
-                //         buffer16 = new Uint8Array(this.buferLength);f
-
-
-                //         offset = 0;
-                //     }
-                // }
-
-
-                // let frames = [...streamParser.parseChunk(value)];
-
-
-                // await this.playerService.mseDecoderService.onstream(frames);
-            } catch (e) {
-                console.error('Error reading stream', e);
-                return;
-            }
-        }
     }
 
     processFlvChunk(chunk: Uint8Array): void {
