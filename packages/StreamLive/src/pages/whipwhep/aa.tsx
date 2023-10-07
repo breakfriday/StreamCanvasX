ff794d31df19e81f011f75f119a33e46aeee5939
break_happy <break_happy@163.com>
2023-10-07 16:29:45 +0800

    feat:whip local_show

--- packages/StreamLive/src/pages/whipwhep/aa.tsx
+++ packages/StreamLive/src/pages/whipwhep/aa.tsx
@@ -14,6 +14,8 @@ const Player: React.FC<Iplayer> = (props) => {
 
     const [options, setOptions] = useState<{url: string; token: string}>({ url: '', token: '' });
 
+    const [buttonStatus, setButtonStatus] = useState({ whip: false, whep: false });
+
     const runWhep = async (value) => {
         // Create peerconnection
      let { url, token } = value;
@@ -60,7 +62,58 @@ const Player: React.FC<Iplayer> = (props) => {
                 setOptions(options_temp);
             }}
           />
-          <Button onClick={() => {
+          <Button
+            disabled={buttonStatus.whip}
+            onClick={() => {
+          const runWhip = async (value) => {
+              // Get mic+cam
+          const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+          let video = videoRef.current!;
+          video.autoplay = true;
+          video.srcObject = stream;
+
+
+          let { url = '', token = '' } = value;
+
+          // Create peerconnection
+          const pc = new RTCPeerConnection();
+
+          // Send all tracks
+          for (const track of stream.getTracks()) {
+              // You could add simulcast too here
+              pc.addTrack(track);
+          }
+
+          // Create whip client
+          const whip = new WHIPClient();
+
+          // const url = 'https://whip.test/whip/endpoint';
+          // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IndoaXAgdGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.jpM01xu_vnSXioxQ3I7Z45bRh5eWRBEY2WJPZ6FerR8';
+
+          // Start publishing
+          whip.publish(pc, url, token);
+
+          debugger;
+
+          let temp = Object.assign({}, buttonStatus, { whep: true });
+
+          debugger;
+
+          setButtonStatus(temp);
+         };
+
+
+         runWhip(options);
+
+            //  openWebcam();
+          }}
+          >
+            push whip
+          </Button>
+          <Button
+            disabled={buttonStatus.whep}
+            onClick={() => {
             let options_temp = options;
 
             runWhep(options_temp);
