70c4e63e5d3a83a894f41524feb857ca88fc30c1
Shiyl <734722984@qq.com>
2023-10-11 15:32:26 +0800

    loadwasm

--- packages/StreamLive/src/pages/webrtc_wsam_canvas/main.js
+++ packages/StreamLive/src/pages/webrtc_wsam_canvas/main.js
@@ -1,14 +1,15 @@
-import loadWASM from './load_wasm.js';
+// import loadWASM from './load_wasm.js';
 import * as tf from '@tensorflow/tfjs';
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import { loadWASM, addScript2 } from '../../../../streamcanvasx/src/utils';
 
 
 let model;
 const Ai = false;
-
+window.Module = {};
 
 const main_player = async () => {
-  let wam;
+  let webdsp_cModule;
   let video;
   let gStream;
   let frameNum;
@@ -16,6 +17,7 @@ const main_player = async () => {
   let gFilter;
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
+  webdsp_cModule = await loadWASM('webdsp_c.js', 'createWebdsp_c', webdsp_cModule);
   model = await cocoSsd.load();
 
   const outlineStuff = (predictions) => {
@@ -46,7 +48,7 @@ const main_player = async () => {
       video.addEventListener('loadeddata', () => {
         canvas.setAttribute('height', video.videoHeight);
         canvas.setAttribute('width', video.videoWidth);
-        gFilter = wam.original;
+        gFilter = webdsp_cModule.original;
         draw();
       });
     }).catch((err) => {
@@ -98,9 +100,9 @@ const main_player = async () => {
   }
 
   function setFilter(evt) {
-    const filter = wam[evt.target.getAttribute('filter')];
-    gFilter = filter || wam.grayScale;
-    debugger
+    const filter = webdsp_cModule[evt.target.getAttribute('filter')];
+    gFilter = filter || webdsp_cModule.grayScale;
+    debugger;
   }
 
   const frameNumDom = document.getElementById('frameNum');
@@ -108,14 +110,161 @@ const main_player = async () => {
     frameNumDom.innerHTML = parseInt(fps);
   }
 
-  // 加载wasm
-  loadWASM().then((module) => {
-    wam = module;
-    // 增加原始滤镜模块
-    wam.original = function (pixels) {
-      return pixels;
+  function bindLastArgs(func, ...boundArgs) {
+    return function (...baseArgs) {
+      return func(...baseArgs, ...boundArgs);
     };
-  });
+  }
+  // 加载wasm;
+  // loadWASM().then((module) => {
+  //   wam = module;
+  //   // 增加原始滤镜模块
+  //   wam.original = function (pixels) {
+  //     return pixels;
+  //   };
+  // });
+  webdsp_cModule['original'] = function (pixels) {
+    return pixels;
+  };
+
+  webdsp_cModule['grayScale'] = function (pixelData) {
+    // 获取像素数组长度
+    const len = pixelData.length;
+    // 开辟一块内存，返回地址
+    const mem = webdsp_cModule._malloc(len);
+    // 把像素数据放入这块内存mem
+    webdsp_cModule.HEAPU8.set(pixelData, mem);
+    // 处理像素，源数据
+    webdsp_cModule._grayScale(mem, len);
+    // 从heap中拿出mem～mem+len（起始指针到位移len的数据）
+    const filtered = webdsp_cModule.HEAPU8.subarray(mem, mem + len);
+    // 释放内存
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  webdsp_cModule['brighten'] = function (pixelData, brightness = 25) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len);
+    webdsp_cModule.HEAPU8.set(pixelData, mem);
+    webdsp_cModule._brighten(mem, len, brightness);
+    const filtered = webdsp_cModule.HEAPU8.subarray(mem, mem + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  webdsp_cModule['invert'] = function (pixelData) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len);
+    webdsp_cModule.HEAPU8.set(pixelData, mem);
+    webdsp_cModule._invert(mem, len);
+    const filtered = webdsp_cModule.HEAPU8.subarray(mem, mem + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  webdsp_cModule['noise'] = function (pixelData) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len * Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule.HEAPF32.set(pixelData, mem / Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule._noise(mem, len);
+    const filtered = HEAPF32.subarray(mem / Float32Array.BYTES_PER_ELEMENT, mem / Float32Array.BYTES_PER_ELEMENT + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  // MultiFilter Family of Functions
+  webdsp_cModule['multiFilter'] = function (pixelData, width, filterType, mag, multiplier, adj) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len);
+    webdsp_cModule.HEAPU8.set(pixelData, mem);
+    webdsp_cModule._multiFilter(mem, len, width, filterType, mag, multiplier, adj);
+    const filtered = webdsp_cModule.HEAPU8.subarray(mem, mem + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  webdsp_cModule['multiFilterFloat'] = function (pixelData, width, filterType, mag, multiplier, adj) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len * Float32Array.BYTES_PER_ELEMENT);
+    HEAPF32.set(pixelData, mem / Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule._multiFilterFloat(mem, len, width, filterType, mag, multiplier, adj);
+    const filtered = HEAPF32.subarray(mem / Float32Array.BYTES_PER_ELEMENT, mem / Float32Array.BYTES_PER_ELEMENT + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  // bindLastArgs is defined and hoisted from below the module load
+  const mag = 127;
+  const mult = 2;
+  const adj = 4;
+  const filt = webdsp_cModule['multiFilter'];
+  const filtFloat = webdsp_cModule['multiFilterFloat'];
+  webdsp_cModule['sunset'] = bindLastArgs(filt, 4, mag, mult, adj);
+  webdsp_cModule['analogTV'] = bindLastArgs(filt, 7, mag, mult, adj);
+  webdsp_cModule['emboss'] = bindLastArgs(filt, 1, mag, mult, adj);
+  webdsp_cModule['urple'] = bindLastArgs(filt, 2, mag, mult, adj);
+  webdsp_cModule['forest'] = bindLastArgs(filtFloat, 5, mag, 3, 1);
+  webdsp_cModule['romance'] = bindLastArgs(filtFloat, 8, mag, 3, 2);
+  webdsp_cModule['hippo'] = bindLastArgs(filtFloat, 2, 80, 3, 2);
+  webdsp_cModule['longhorn'] = bindLastArgs(filtFloat, 2, 27, 3, 2);
+  webdsp_cModule['underground'] = bindLastArgs(filt, 8, mag, 1, 4);
+  webdsp_cModule['rooster'] = bindLastArgs(filt, 8, 60, 1, 4);
+  webdsp_cModule['moss'] = bindLastArgs(filtFloat, 1, mag, 1, 1);
+  webdsp_cModule['mist'] = bindLastArgs(filt, 1, mag, 1, 1);
+  webdsp_cModule['kaleidoscope'] = bindLastArgs(filt, 1, 124, 4, 3);
+  webdsp_cModule['tingle'] = bindLastArgs(filtFloat, 1, 124, 4, 3);
+  webdsp_cModule['bacteria'] = bindLastArgs(filt, 4, 0, 2, 4);
+  webdsp_cModule['hulk'] = bindLastArgs(filt, 2, 10, 2, 4);
+  webdsp_cModule['ghost'] = bindLastArgs(filt, 1, 5, 2, 4);
+  webdsp_cModule['swebdsp_cModulep'] = bindLastArgs(filtFloat, 1, 40, 2, 3);
+  webdsp_cModule['twisted'] = bindLastArgs(filt, 1, 40, 2, 3);
+  webdsp_cModule['security'] = bindLastArgs(filt, 1, 120, 1, 0);
+  webdsp_cModule['robbery'] = bindLastArgs(filtFloat, 1, 120, 1, 0);
+  // end filters from multiFilter family
+
+  webdsp_cModule['sobelFilter'] = function (pixelData, width, height, invert = false) {
+    const len = pixelData.length;
+    const mem = webdsp_cModule._malloc(len);
+    webdsp_cModule.HEAPU8.set(pixelData, mem);
+    webdsp_cModule._sobelFilter(mem, width, height, invert);
+    const filtered = webdsp_cModule.HEAPU8.subarray(mem, mem + len);
+    webdsp_cModule._free(mem);
+    return filtered;
+  };
+  // convFilter family of filters
+  webdsp_cModule['convFilter'] = function (pixelData, width, height, kernel, divisor, bias = 0, count = 1) {
+    const arLen = pixelData.length;
+    const memData = webdsp_cModule._malloc(arLen * Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule.HEAPF32.set(pixelData, memData / Float32Array.BYTES_PER_ELEMENT);
+    const kerWidth = kernel[0].length;
+    const kerHeight = kernel.length;
+    const kerLen = kerWidth * kerHeight;
+    const flatKernel = kernel.reduce((acc, cur) => acc.concat(cur));
+    const memKernel = webdsp_cModule._malloc(kerLen * Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule.HEAPF32.set(flatKernel, memKernel / Float32Array.BYTES_PER_ELEMENT);
+    webdsp_cModule._convFilter(memData, width, height, memKernel, 3, 3, divisor, bias, count);
+    const filtered = webdsp_cModule.HEAPF32.subarray(memData / Float32Array.BYTES_PER_ELEMENT, memData / Float32Array.BYTES_PER_ELEMENT + arLen);
+    webdsp_cModule._free(memData);
+    webdsp_cModule._free(memKernel);
+    return filtered;
+  };
+  // defining kernel and other parameters before each function definition
+  let kernel = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+  let divisor = 9;
+  const bias = 0;
+  const count = 1;
+  const conv = webdsp_cModule['convFilter'];
+  webdsp_cModule['blur'] = bindLastArgs(conv, kernel, divisor, bias, 3);
+  kernel = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], divisor = 1;
+  webdsp_cModule['strongSharpen'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]], divisor = 2;
+  webdsp_cModule['sharpen'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[1, -1, -1], [-1, 8, -1], [-1, -1, 1]], divisor = 3;
+  webdsp_cModule['clarity'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[-1, -1, 1], [-1, 14, -1], [1, -1, -1]], divisor = 3;
+  webdsp_cModule['goodMorning'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[4, -1, -1], [-1, 4, -1], [0, -1, 4]], divisor = 3;
+  webdsp_cModule['acid'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[0, 0, -1], [-1, 12, -1], [0, -1, -1]], divisor = 2;
+  webdsp_cModule['dewdrops'] = bindLastArgs(conv, kernel, divisor);
+  kernel = [[-1, -1, 4], [-1, 9, -1], [0, -1, 0]], divisor = 2;
+  webdsp_cModule['destruction'] = bindLastArgs(conv, kernel, divisor);
+
   // 加载摄像头图像
   document.getElementById('open').addEventListener('click', openWebcam);
   document.getElementById('close').addEventListener('click', stopWebcam);
--- packages/streamcanvasx/src/services/video/load_wasm.js
+++ packages/streamcanvasx/src/services/video/load_wasm.js
@@ -1,45 +1,48 @@
-const Module = {};
-let script;
-export default function loadWASM() {
-  return new Promise((resolve, reject) => {
-    self.fetch('./watermark.wasm')
-        .then((response) => response.arrayBuffer())
-        .then((buffer) => {
-          Module.wasmBinary = buffer;
-          const script = document.createElement('script');
-          script.onload = buildWam;
-          script.src = './watermark.js';
-          // script.src = 'watermark.js';
-          document.body.appendChild(script);
+// const Module = {};
+// window.watermark = {};
+// let script;
+// export default function loadWASM() {
+//   return new Promise((resolve, reject) => {
+//     self.fetch('./watermark.wasm')
+//         .then((response) => response.arrayBuffer())
+//         .then((buffer) => {
+//           Module.wasmBinary = buffer;
+//           const script = document.createElement('script');
+//           script.onload = buildWam;
+//           script.src = './watermark.js';
+//           // script.src = 'watermark.js';
+//           document.body.appendChild(script);
 
-          function buildWam() {
-            console.log('Emscripten boilerplate loaded.');
-            const wam = {};
-            wam['watermarkArnoldDCT'] = function (pixelData, width, height, watermarkData, size, count) {
-              const len = pixelData.length;
-              const len2 = watermarkData.length;
-              const mem = _malloc(len + len2);
-              HEAPU8.set(pixelData, mem);
-              HEAPU8.set(watermarkData, mem + len);
-              _watermarkArnoldDCT(mem, width, height, mem + len, size, count);
-              const filtered = HEAPU8.subarray(mem, mem + len);
-              _free(mem);
-              return filtered;
-            };
-            wam['watermarkArnoldIDCT'] = function (pixelData, width, height, watermarkData, size, count) {
-              const len = pixelData.length;
-              const len2 = size * size * 4;
-              const mem = _malloc(len + len2);
-              HEAPU8.set(pixelData, mem);
-              HEAPU8.set(watermarkData, mem + len);
-              _watermarkArnoldIDCT(mem, width, height, mem + len, size, count);
-              // const filtered = HEAPU8.subarray(mem, mem+len);
-              const filtered2 = HEAPU8.subarray(mem + len, mem + len + len2);
-              _free(mem);
-              return filtered2;
-            };
-            resolve(wam);
-          }
-      });
-  });
-}
+//           async function buildWam() {
+//             console.log('Emscripten boilerplate loaded.');
+//             window.watermark = await createWatermark();
+//             const wam = {};
+//             wam['watermarkArnoldDCT'] = function (pixelData, width, height, watermarkData, size, count) {
+//               const len = pixelData.length;
+//               const len2 = watermarkData.length;
+//               const mem = window.watermark._malloc(len + len2);
+//               window.watermark.HEAPU8.set(pixelData, mem);
+//               window.watermark.HEAPU8.set(watermarkData, mem + len);
+//               window.watermark._watermarkArnoldDCT(mem, width, height, mem + len, size, count);
+//               const filtered = window.watermark.HEAPU8.subarray(mem, mem + len);
+//               window.watermark._free(mem);
+//               return filtered;
+//             };
+//             wam['watermarkArnoldIDCT'] = function (pixelData, width, height, watermarkData, size, count) {
+//               const len = pixelData.length;
+//               const len2 = size * size * 4;
+//               const mem = window.watermark._malloc(len + len2);
+//               window.watermark.HEAPU8.set(pixelData, mem);
+//               window.watermark.HEAPU8.set(watermarkData, mem + len);
+//               window.watermark._watermarkArnoldIDCT(mem, width, height, mem + len, size, count);
+//               // const filtered = HEAPU8.subarray(mem, mem+len);
+//               const filtered2 = window.watermark.HEAPU8.subarray(mem + len, mem + len + len2);
+//               window.watermark._free(mem);
+//               return filtered2;
+//             };
+
+//             resolve(wam);
+//           }
+//       });
+//   });
+// }
