278c59e9a27ae5d60d635b16a7a94aa5c7058662
break_happy <break_happy@163.com>
2023-10-19 16:25:28 +0800

    删除资源大文件 提高 clone 和 fetch 的速度

--- packages/streamcanvasx/es2017/services/orignClass.d.ts
+++
@@ -1,6 +0,0 @@
-import { ServiceA } from './ServiceA';
-export declare class OriginSerivce {
-    logger: ServiceA;
-    constructor(logger: ServiceA, params: any);
-    doSomething(): void;
-}
--- packages/streamcanvasx/esm/container.js
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import { Container } from "inversify";
-// import Logger from './Logger';
-// import orignClass from './orignClass';
-var container1 = new Container();
-var containerPlayer = new Container();
-// container.bind<Logger>(Logger).toSelf();
-// container.bind<orignClass>(orignClass).toSelf();
-export { container1, Container, containerPlayer };
