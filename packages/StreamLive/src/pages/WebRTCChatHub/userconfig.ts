a199e312a9b2384fd5cc7db1fef2a1c3222668f7
break_happy <break_happy@163.com>
2023-12-04 11:48:50 +0800

    feat:update

--- packages/StreamLive/src/pages/WebRTCChatHub/userconfig.ts
+++ packages/StreamLive/src/pages/WebRTCChatHub/userconfig.ts
@@ -1,14 +1,14 @@
 let userList = [
-    { name: '测试1', deviceId: '1', pwd: 123 },
-    { name: '测试2', deviceId: '2', pwd: 123 },
-    { name: '测试3', deviceId: '3', pwd: 123 },
-    { name: '测试4', deviceId: '4', pwd: 123 },
-    { name: '测试5', deviceId: '5', pwd: 123 },
-    { name: '测试6', deviceId: '6', pwd: 123 },
-    { name: '测试7', deviceId: '7', pwd: 123 },
-    { name: '测试8', deviceId: '8', pwd: 123 },
-    { name: '测试9', deviceId: '9', pwd: 123 },
-    { name: '测试10', deviceId: '10', pwd: 123 },
+    { name: 'test1', deviceId: 'test1', pwd: 123 },
+    { name: 'test2', deviceId: 'test2', pwd: 123 },
+    { name: 'test3', deviceId: 'test3', pwd: 123 },
+    { name: 'test4', deviceId: 'test4', pwd: 123 },
+    { name: 'test5', deviceId: 'test5', pwd: 123 },
+    { name: 'test6', deviceId: 'test6', pwd: 123 },
+    { name: 'test7', deviceId: 'test7', pwd: 123 },
+    { name: 'test8', deviceId: 'test8', pwd: 123 },
+    { name: 'test9', deviceId: 'test9', pwd: 123 },
+    { name: 'test0', deviceId: 'test10', pwd: 123 },
 
 ];
 
