0ede1b95db6193236792db35ddd4b91e7347457b
break_happy <break_happy@163.com>
2023-12-19 16:39:21 +0800

    feat:update

--- packages/streamcanvasx/src/index.ts
+++ packages/streamcanvasx/src/index.ts
@@ -1,6 +1,33 @@
 
 import { createPlayerServiceInstance } from './serviceFactories/createPlayerInstance';
 
+
+type ICreatePlayerServiceInstance = ReturnType<typeof createPlayerServiceInstance>;
+
+let loadMicroModule = async function (vendorUrl: string, indexUrl: string): Promise<any> {
+    try {
+        // 首先加载vendor模块
+        await import(/* webpackIgnore: true */ vendorUrl);
+
+        // vendor模块加载完成后，加载index模块
+        const module = await import(/* webpackIgnore: true */ indexUrl);
+        return module;
+    } catch (error) {
+        console.error('Error loading micro module:', error);
+        throw new Error('Failed to load micro module');
+    }
+};
+
+
+const createPlayerServicePromise = async function (parm): Promise<ICreatePlayerServiceInstance> {
+    let module = await loadMicroModule(
+        'https://breakhappy.oss-cn-beijing.aliyuncs.com/streamcanvasx/0.1.78/index.esm.es5.production.js',
+        'https://breakhappy.oss-cn-beijing.aliyuncs.com/streamcanvasx/0.1.78/index.esm.es5.production.js');
+
+    return module.createPlayerServiceInstance(parm);
+};
+
 export {
     createPlayerServiceInstance,
+    createPlayerServicePromise,
 };
