00de9cd5830dd53d03391be43fba6e5b7daa97bb
break_happy <break_happy@163.com>
2023-12-26 17:20:03 +0800

    feat:update

---
+++ packages/streamcanvasx/src/services/audio/scheduler.ts
@@ -0,0 +1,47 @@
+
+class Scheduler {
+    private taskQueue: (() => Promise<any>)[] = []; // 任务队列
+    private concurrentLimit: number;
+    private runningTasks = 0; // 当前并发数
+
+    constructor(concurrentLimit: number) {
+        this.concurrentLimit = concurrentLimit;
+    }
+
+    addTask(task: () => Promise<any>) {
+        this.taskQueue.push(task); // 将任务添加到队列中
+        this.runNextTask();
+    }
+
+    private runNextTask() {
+        if (this.runningTasks < this.concurrentLimit && this.taskQueue.length > 0) {
+            const task = this.taskQueue.shift();
+            if (task) {
+                this.runningTasks++;
+                task().then((result) => {
+                    this.runningTasks--;
+                    if (result === 'clean') {
+                        console.log(' Scheduler clearQueu ');
+                        this.clearQueue();
+                    } else {
+                        this.runNextTask();
+                    }
+                });
+            }
+        }
+    }
+    getQueue() {
+        return this.taskQueue;
+    }
+
+    clearQueue() {
+        this.taskQueue = []; // 清空队列
+    }
+}
+
+export default Scheduler;
+
+
+// const scheduler = new Scheduler(3); // 设置并发限制为3
+
+// scheduler.addTask(() => new Promise(resolve => setTimeout(resolve, 1000, 'Task 1 Completed')));
