74d6b06002302e5dd6d3d588c30adc6eaac403ac
break_happy <break_happy@163.com>
2024-01-04 13:42:56 +0800

    feat:update

--- packages/streamcanvasx/src/services/player/index.ts
+++ packages/streamcanvasx/src/services/player/index.ts
@@ -137,7 +137,7 @@ class PlayerService extends Emitter {
             vbps: 0, // 当前视频码率，单位bit
             ts: 0, // 当前视频帧pts，单位毫秒
         };
-        window.streamCanvasX = window.__VERSION__ || 'DEV_0.1.84';
+        window.streamCanvasX = window.__VERSION__ || 'DEV_0.1.85';
     }
 
     init(config?: IplayerConfig) {
