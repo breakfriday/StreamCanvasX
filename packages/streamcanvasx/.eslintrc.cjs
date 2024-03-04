ec41a1c209cd874dd758669dba31777e91d3d816
break_happy <break_happy@163.com>
2024-03-04 17:46:25 +0800

    feat : 關閉駝峰規則

--- packages/streamcanvasx/.eslintrc.cjs
+++ packages/streamcanvasx/.eslintrc.cjs
@@ -1,5 +1,15 @@
+// const { getESLintConfig } = require('@applint/spec');
+
+// module.exports = getESLintConfig(
+//   'common-ts',
+// );
+
+
 const { getESLintConfig } = require('@applint/spec');
 
-module.exports = getESLintConfig(
-  'common-ts',
-);
+const config = getESLintConfig('common-ts');
+
+// 覆盖或添加规则
+config.rules['camelcase'] = 'off'; // 关闭 camelcase 规则
+
+module.exports = config;
