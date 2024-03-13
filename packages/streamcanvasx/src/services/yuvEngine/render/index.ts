a582dce45dc223dc011fa05d18742fa17a9dc093
break_happy <break_happy@163.com>
2024-03-13 15:54:25 +0800

    feat:update

--- packages/streamcanvasx/src/services/yuvEngine/render/index.ts
+++ packages/streamcanvasx/src/services/yuvEngine/render/index.ts
@@ -3,6 +3,15 @@ import { injectable, inject, Container, LazyServiceIdentifer } from 'inversify';
 import PlayerService from '../player/index';
 
 import createREGL from 'regl';
+
+interface YUVFrame {
+  yData: Uint8Array;
+  uData: Uint8Array;
+  vData: Uint8Array;
+  width: number;
+  height: number;
+}
+
 class YuvEnging {
     canvas_el: HTMLCanvasElement;
     regGl: createREGL.Regl;
@@ -33,16 +42,16 @@ class YuvEnging {
         contentEl.append(this.canvas_el);
         this.setCanvasSize();
     }
-    initGl() {
 
-    }
 
     initRegl() {
         this.regGl = createREGL({ canvas: this.canvas_el, extensions: ['OES_texture_float', 'angle_instanced_arrays'] });
         let regl=this.regGl;
-        const textureY = regl.texture(); // 存储视频帧的亮度（Y分量）信息
-        const textureU = regl.texture(); // 存储色度信息
-        const textureV = regl.texture(); // 存储色度信息
+        const textureY = regl.texture({ width: 1, height: 1, format: 'luminance' }); // 存储视频帧的亮度（Y分量）信息
+        const textureU = regl.texture({ width: 1, height: 1, format: 'luminance' }); // 存储色度信息
+        const textureV = regl.texture({ width: 1, height: 1, format: 'luminance' }); // 存储色度信息
+
+
         this.yuvTexture={ textureU,textureY,textureV };
 
         // 将yuv 数据转换为RGB
@@ -57,7 +66,7 @@ class YuvEnging {
                 float y = texture2D(textureY, uv).r;  //从textureY纹理中根据uv坐标采样颜色，然后提取其RGB颜色值存储到变量y中
                 float u = texture2D(textureU, uv).r - 0.5; 
                 float v = texture2D(textureV, uv).r - 0.5;
-                gl_FragColor = vec4(y + 1.403 * v, y - 0.344 * u - 0.714 * v, y + 1.77 * u, 1.0);  // 最终 计算转换成rgb  1.0 表示不透明
+                gl_FragColor = vec4(y + 1.403 * v, y - 0.344 * u - 0.714 * v, y + 1.77 * u, 1.0);  // 最终 標準公式 计算转换成rgb  1.0 表示不透明
                 
               }
             `,
@@ -76,9 +85,12 @@ class YuvEnging {
                 2, 2]
             },
             uniforms: {
-              textureY: () => textureY,
-              textureU: () => textureU,
-              textureV: () => textureV
+              // textureY: () => textureY,
+              // textureU: () => textureU,
+              // textureV: () => textureV
+              textureY: regl.prop('textureY'),
+              textureU: regl.prop('textureU'),
+              textureV: regl.prop('textureV'),
             },
             count: 3
           });
@@ -101,11 +113,14 @@ class YuvEnging {
     }
 
 
-    update_yuv_texture(yData: Uint8Array, uData: Uint8Array, vData: Uint8Array, width: number, height: number) {
-          this.yuvTexture.textureY({ data: yData, width, height, format: 'luminance' });
-          this.yuvTexture.textureU({ data: uData, width: width / 2, height: height / 2, format: 'luminance' });
-          this.yuvTexture.textureV({ data: vData, width: width / 2, height: height / 2, format: 'luminance' });
-    }
+    update_yuv_texture(yuvFrame: YUVFrame) {
+      if(yuvFrame) {
+        let { yData, uData, vData,width,height }=yuvFrame;
+        this.yuvTexture.textureY({ data: yData, width, height, format: 'luminance' });
+        this.yuvTexture.textureU({ data: uData, width: width / 2, height: height / 2, format: 'luminance' });
+        this.yuvTexture.textureV({ data: vData, width: width / 2, height: height / 2, format: 'luminance' });
+      }
+}
 
     render() {
       let regl=this.regGl;
@@ -114,7 +129,12 @@ class YuvEnging {
           color: [0, 0, 0, 1],
           depth: 1,
         });
-        this.drawCommand();
+        this.drawCommand({
+          textureY: this.yuvTexture.textureY,
+          textureU: this.yuvTexture.textureU,
+          textureV: this.yuvTexture.textureV
+
+        });
       });
     }
 
