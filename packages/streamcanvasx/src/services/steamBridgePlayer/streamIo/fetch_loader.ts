7fb1e4b563deda3ca557879f11695b38364d2943
break_happy <break_happy@163.com>
2024-03-25 18:32:17 +0800

    feat:update

--- packages/streamcanvasx/src/services/steamBridgePlayer/streamIo/fetch_loader.ts
+++ packages/streamcanvasx/src/services/steamBridgePlayer/streamIo/fetch_loader.ts
@@ -1,13 +1,45 @@
-class FetchIo {
+import PlayerService from '../index';
+class FetchLoader {
+    playerService: PlayerService;
+    private _abortController: AbortController;
     constructor() {
 
     }
-    init() {
+    get abortController() {
+        return this._abortController;
+    }
+    init(playerService: PlayerService) {
+        this.playerService=playerService;
+    }
+    open() {
+        this.fetch_io();
+    }
+
+    async fetch_io() {
+        let { url } = this.playerService.config;
+
+        let headers = new Headers();
+        this._abortController = new AbortController();
+        let params: RequestInit = {
+            method: 'GET',
+            mode: 'cors', // cors is enabled by default
+            credentials: 'same-origin', // withCredentials is disabled by default
+            headers: headers,
+            cache: 'default',
+            referrerPolicy: 'no-referrer-when-downgrade',
+            signal: this.abortController.signal,
+
+        };
+    }
+    abort() {
 
     }
-    load() {
+    detroy() {
+
+    }
+    process() {
 
     }
 }
 
-export default FetchIo;
+export default FetchLoader;
---
+++ packages/streamcanvasx/src/services/steamBridgePlayer/streamIo/index.ts
@@ -0,0 +1,27 @@
+import { injectable, inject, Container, LazyServiceIdentifer } from 'inversify';
+import PlayerService from '../index';
+import FetchLoader from "./fetch_loader";
+class StreamIo {
+    _ioLoader: FetchLoader
+    playerService: PlayerService
+    constructor() {
+
+    }
+    init(playerService: PlayerService) {
+        this.playerService=playerService;
+        this._ioLoader=new FetchLoader();
+        this._ioLoader.init(playerService);
+    }
+    open() {
+        this._ioLoader.open();
+    }
+    abort() {
+
+    }
+
+    process() {
+
+    }
+}
+
+export default StreamIo;
